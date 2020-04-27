204bc843ab0408227039f149ed4b715fb540b3df
Doug Hoard <doug.hoard@gmail.com>
2020-04-27 11:32:44 -0400

    Fixed Schema Registry run shell script

--- assets/schema-registry-run.sh
+++ assets/schema-registry-run.sh
@@ -2,4 +2,4 @@
 
 export LOG_DIR=./logs
 
-/opt/confluent/bin/schema-registry-server-start /opt/confluent/etc/schema-registry/schema-registry.properties
+/opt/confluent/bin/schema-registry-start /opt/confluent/etc/schema-registry/schema-registry.properties
