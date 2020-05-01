6b13b4a651d1412720939982d234a71331d49e2b
Doug Hoard <doug.hoard@gmail.com>
2020-05-01 11:31:50 -0400

    Removed creation of logs directory for certain components

--- assets/ksql-run.sh
+++ assets/ksql-run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-mkdir -p ./logs
-export LOG_DIR=./logs
-
 /opt/confluent/bin/ksql-server-start /opt/confluent/etc/ksqldb/ksql-production-server.properties >> ksql.log 2>&1
--- assets/schema-registry-run.sh
+++ assets/schema-registry-run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-mkdir -p ./logs
-export LOG_DIR=./logs
-
 /opt/confluent/bin/schema-registry-start /opt/confluent/etc/schema-registry/schema-registry.properties >> schema-registry.log 2>&1
