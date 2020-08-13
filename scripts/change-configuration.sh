42d00d3f798d91cfdfa24d4f06ec7b2aa558ea2b
Doug Hoard <doug.hoard@gmail.com>
2020-08-13 10:57:15 -0400

    Updated code to append new values

--- scripts/change-configuration.sh
+++ scripts/change-configuration.sh
@@ -44,6 +44,6 @@ WORKING_DIRECTORY=${PWD}
 
 log_banner "INFO" "BEGIN Configuration changes ..."
 
-java -jar ./tools/value-replacer-0.1.0-SNAPSHOT.jar "./tools/value-replacer-$VERSION.txt"
+java -jar ./tools/value-replacer-0.2.0.jar "./tools/value-replacer-$VERSION.txt"
 
 log_banner "INFO" "END Configuration changes"
