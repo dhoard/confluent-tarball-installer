2487e7592fc3a0ff787044fb41a2a96042518c60
Doug Hoard <doug.hoard@gmail.com>
2020-08-26 13:07:06 -0400

    Added code to delete kafka-connect.log

--- assets/confluent_delete_logs.sh
+++ assets/confluent_delete_logs.sh
@@ -4,6 +4,8 @@ rm -Rf /home/cp-kafka/zookeeper.log
 rm -Rf /home/cp-kafka/zookeeper-logs/*
 rm -Rf /home/cp-kafka/kafka.log
 rm -Rf /home/cp-kafka/kafka-logs/*
+rm -Rf /home/cp-kafka-connect/kafka-connect.log
+rm -Rf /home/cp-kafka/kafka-logs/*
 rm -Rf /home/cp-ksql/ksql.log
 rm -Rf /home/cp-ksql/logs/*
 rm -Rf /home/cp-control-center/control-center.log
